af83882ac0773812c18609f2eecb67ee301abe74
uczeń <ucze@sala213_1>
2015-11-13 11:26:08 +0100

    Zrobienie pierwszej próby

--- index.php
+++ index.php
@@ -1,17 +1,75 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<!doctype html>
+
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+  
+  <head>
+    <title>Moodle!</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+  <style>
+    .odstep{
+      height: 15px;
+    }
+    </style>
+    
+    <style>
+    body{
+        background-image: url('zdjecia/tlo.jpg');
+        padding:15px;
+        background-color:#DFF;
+      }
+      
+  </style>
+  
+  </head>
+  
+  <body>
+  <div class="odstep"></div>   
+  <div class="container">
+      <div class="row">
+
+       <div class="col-md-8">
+          <img src="https://builder.divshot.com/img/placeholder-100x100.gif" width="300" height="100">
+        </div>
+        <div class="col-md-4">
+          <div id="gorny">
+            <button type="submit" name="submit" class="btn btn-block btn-success btn-lg">Zaloguj</button>
+          </div>
+          <div id="dolny">
+            <button type="button" name="przypomnij" class="btn btn-block btn-danger btn-lg">Rejestracja</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div class="odstep"></div> 
+  <div class="odstep"></div>   
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>Logowanie:</h1>
+  
+  <div class="form-group">
+  <label class="control-label">Login</label>
+  <div class="controls"><input type="text" class="form-control"></div>
+  </div>
+  <div class="form-group">
+  <label class="control-label">Hsalo</label>
+  <div class="controls"><input type="text" class="form-control"></div>
+</div>
+
+          <div id='l_button_lewy'><button type="submit" name="submit" class="btn btn-info btn-block ">Zaloguj</button></div>
+          
+            <div id='l_button_prawy'><button type="button" name="przypomnij" class="btn  btn-block">Przypomnij haslo</button></div>
+        </div>
+        <div class="col-md-6">
+          <h1>Witaj na moodle!</h1>
+          <p>Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here.Enter your paragraph text here. nter your paragraph text here.Enter your paragraph text here. nter your paragraph text here.Enter your paragraph text here. nter your paragraph text here.Enter your paragraph text here.nter your paragraph text here.Enter your paragraph text here.nter your paragraph text here.Enter your paragraph text here.nter your paragraph text here.Enter your paragraph text here.</p>
+        
+        </div>
+      </div>
+    </div>
+  </body>
+
 </html>
